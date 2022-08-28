e8c89d94fa5f82a1e0b814cf30d4de2a05b7705c
Ahmad-045 <ahmad.amin@devsinc.com>
2022-08-28 23:47:21 +0500

    Admin search button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    if !params[:filter] || (params[:filter].present? && params[:filter] == 'Voters')
-      @users = User.all
-    elsif params[:filter] == 'Candidate'
+    if params[:filter].present? && params[:filter] == 'Voters'
+      @users = User.page(params[:page]).per(10)
+    elsif !params[:filter] || params[:filter] == 'Candidate'
       @users = Candidate.user_details(Candidate.pluck(:user_id))
     end
     @filter = params[:filter]
