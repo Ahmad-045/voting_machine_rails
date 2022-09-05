d9bdc04be02b03ca168ac68da437b112c8aa2c4c
Ahmad-045 <ahmad.amin@devsinc.com>
2022-09-05 19:21:30 +0500

    Changing host to check

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'voting-machine-0045.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
 
